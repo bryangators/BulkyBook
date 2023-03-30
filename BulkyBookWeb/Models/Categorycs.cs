ba19a28f5e8df97df319209fb8e79c2b37f63cd6
Bryan Kristofferson <bryangators510@gmail.com>
2023-03-29 20:02:03 -0400

    Added Category model

---
+++ BulkyBookWeb/Models/Categorycs.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Categorycs
+    {
+        #region Properties
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+        #endregion
+
+    }
+}
