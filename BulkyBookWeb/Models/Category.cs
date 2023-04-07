f73022daad6266ecbc9fecf0c7da8f7ca954122c
Bryan Kristofferson <bryangators510@gmail.com>
2023-04-06 21:05:30 -0400

    Updated Category Model and View Added bootstrap lux theme Modified _layout added create category page and new action

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -9,7 +10,8 @@ namespace BulkyBookWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        public string DisplayOrder { get; set; }
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
         public DateTime CreateDateTime { get; set; } = DateTime.Now;
         #endregion
 
