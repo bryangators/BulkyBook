2e0aabdb9e1348d253816be6be1dad6ff86555d5
Bryan Kristofferson <bryangators510@gmail.com>
2023-04-11 19:53:38 -0400

    Setup Razor page demo database and seeded data Added categories view

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        #region Properties
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display order must be betweeen 1-100")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+        #endregion
+    }
+}
